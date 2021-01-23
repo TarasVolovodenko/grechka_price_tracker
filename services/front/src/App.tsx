81dfd7049b477a1a948ebce8cd5351963a13689c
itdaos <jjok730@gmail.com>
2021-01-24 00:00:02 +0200

    Request base

--- services/front/src/App.tsx
+++ services/front/src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import ItemComponent from './Item';
+import axios from 'axios';
+import { throws } from 'assert';
 
-function App() {
-  return (
-    <p>Grechka Prices</p>
-  );
+
+interface Item {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+
+class App extends React.Component<{}, {items: ItemComponent[], page: number}> {
+  
+  constructor() {
+    super({})
+    this.state = {
+      items: [],
+      page: 1,
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>{
+      this.setState({items: 
+        res.data.map( (item: Item) =>  <ItemComponent id={item.id} title={item.title} body={item.body} userId={item.userId} /> )
+      })
+    })
+    .catch(err=>console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.items.map( (item) => item)}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ services/front/src/Item.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class ItemComponent extends React.Component<{
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }> {
+
+    render() {
+        return (
+            <div className="container container-md">
+            <div className="row" >
+  
+              <div className="col-md-2 col-sm-2 col-4 col-xs-6 col1 align-self-center" >
+                <img src = "favicon.ico" />
+              </div>
+  
+              <div className= "col col-md-10 col-sm-10 col-8 col-xs-6 col2 align-self-center">
+                <div className= "row">
+                  <div className= "new-r w-100"></div>
+                  <div className="col">
+                    <p className= "name">{this.props.body}</p>
+                  </div>
+                  <div className= "new-r w-100"></div>
+                  <div className="col">
+                    <p>{this.props.id}</p>
+                  </div>
+                  <div className= "new-r w-100"></div>
+                  <div className="col">
+                    <p>{this.props.userId}</p>
+                  </div>
+                  <div className= "new-r w-100"></div>
+                  <div className="col">
+                    <p>{this.props.title}</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+          </div>
+        );
+    }
+}
+
+export default ItemComponent
