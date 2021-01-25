f719ebac90b42f2cac1158171656d8a05ce3f1e7
supruniuk <juliasupruniuk23@gmail.com>
2021-01-25 20:16:00 +0200

    msg

--- services/front/src/Item.tsx
+++ services/front/src/Item.tsx
@@ -13,11 +13,11 @@ class ItemComponent extends React.Component<{
 
     render() {
         return (
-            <a className="container container-md item" href={this.props.website_url}>
+            <a className="container container-md item main-a">
             <div className="row" >
   
               <div className="col-md-2 col-sm-2 col-4 col-xs-6 col1 align-self-center" >
-                <a className = "imlink" href={this.props.website_url}><img src={this.props.image_link} alt={this.props.title}/></a>
+                <a className = "imlink" href={this.props.website_url} target="_blank" ><img src={this.props.image_link} alt={this.props.title}/></a>
               </div>
   
               <div className= "col col-md-10 col-sm-10 col-8 col-xs-6 col2 align-self-center">
@@ -28,11 +28,11 @@ class ItemComponent extends React.Component<{
                   </div>
                   <div className= "new-r w-100"></div>
                   <div className="col">
-                    <p>{this.props.cost}</p>
+                    <p>{this.props.weight}</p>
                   </div>
                   <div className= "new-r w-100"></div>
                   <div className="col">
-                    <p>{this.props.weight}</p>
+                    <p>{this.props.price}</p>
                   </div>
                   <div className= "new-r w-100"></div>
                   <div className="col">
